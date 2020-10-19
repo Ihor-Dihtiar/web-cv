4877677aa164cc21185a613a316ac53d529382b1
Igor Digtiar <ihordihtiar@gmail.com>
2020-10-19 22:55:49 +0300

    section about me

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,3 +1,20 @@
 import '../sass/index.scss';
 
 console.log('Congratulate! This script is working!')
+
+window.onload=function(){
+    const menuBtn = document.querySelector('.btn-burger');
+    const menuBtnIcon = document.querySelector('.burger-icon');
+    const menu = document.querySelector('.side-menu');
+    const shift = document.querySelectorAll('.shift');
+    
+    
+    menuBtn.addEventListener('click', ()=>{
+        menuBtnIcon.classList.toggle('open');
+        menu.classList.toggle('open');
+        shift.forEach(element => {
+           element.classList.toggle('open'); 
+        });
+        
+    })
+}
